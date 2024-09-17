36c4f22e4b75a7c5d67408037d93758965394679
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-17 09:17:53 -0400

    add iterator

--- src/hashmap.f90
+++ src/hashmap.f90
@@ -120,6 +120,17 @@ module hashmap_mod
     end subroutine internal_hashmap_clear
 
 
+    function internal_hashmap_iter(map, i, item) result(has_item) bind(c, name = "hashmap_iter")
+      use, intrinsic :: iso_c_binding
+      implicit none
+
+      type(c_ptr), intent(in), value :: map
+      integer(c_size_t), intent(inout) :: i
+      type(c_ptr), intent(inout) :: item
+      logical(c_bool) :: has_item
+    end function internal_hashmap_iter
+
+
 !? FUNCTION BLUEPRINTS. ===========================================================================
 
 
