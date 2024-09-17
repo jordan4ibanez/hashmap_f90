26b06ac38e3850f0f45423dbda3b19132eab087b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-17 10:36:33 -0400

    remove this mess

--- test/protyping.f90
+++ test/protyping.f90
@@ -74,35 +74,4 @@ program prototyping
     end select
   end do
 
-  ! type(c_ptr) :: map, hash_data_loc
-  ! character(len = :, kind = c_char), allocatable, target :: hash_key
-  ! integer(c_int64_t) :: hash_result
-  ! type(c_ptr) :: output
-  ! type(cool), pointer :: test_data
-
-  ! type(c_ptr) :: test_output_c_pointer
-  ! type(cool), pointer :: test_output_data
-
-
-  ! map = internal_hashmap_new(sizeof(cool()), 0_8, 0_8, 0_8, c_funloc(debug_hash_function), c_funloc(debug_compare_function), c_null_ptr, c_null_ptr)
-
-  ! hash_key = "hi there"
-
-  ! hash_result = hashmap_xxhash3(c_loc(hash_key), int(len(hash_key), c_size_t), 0_8, 0_8)
-
-  ! allocate(test_data)
-  ! test_data%i = 555
-
-  ! !* Can do c_associated to check needed deallocation.
-  ! output = internal_hashmap_set(map, c_loc(test_data))
-
-  ! print*,output
-
-  ! test_output_c_pointer = internal_hashmap_get_with_hash(map, )
-
-
-  ! call internal_hashmap_free(map)
-
-
-
 end program prototyping
