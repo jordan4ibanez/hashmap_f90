d1c9113be751e9be71ae0ffc8a6dcbbb5f87406f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-17 12:45:39 -0400

    this looks silly but it's EXTREMELY FAST!

--- src/hashmap_types.f90
+++ src/hashmap_types.f90
@@ -12,4 +12,12 @@ module hashmap_types
   end type element_s_key
 
 
+  !* Element in the hashmap.
+  !* 48 bytes.
+  type :: element_i_key
+    integer(c_int64_t) :: key = 0
+    class(*), pointer :: data => null()
+  end type element_i_key
+
+
 end module hashmap_types
