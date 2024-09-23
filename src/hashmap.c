ee612d5fc5d51d45a731040858d2f84cb2959693
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-23 09:28:23 -0400

    And use it

--- src/hashmap.c
+++ src/hashmap.c
@@ -384,21 +384,10 @@ static bool resize(struct hashmap *map, size_t new_cap)
  */
 const void *hashmap_set_str_key(struct hashmap *map, const char *key_s, size_t string_length, const void *raw_item)
 {
-    header header_element;
-
     //! The string length will be checked in fortran.
 
-    // Set header parameters.
-    header_element.is_string = true;
-    header_element.string_length = string_length;
-
-    // Jump over the first two bytes and memcpy the data into the key.
-    memcpy(&header_element + 2, key_s, string_length);
-
-    // Jump over the entire string and create a null terminator.
-    //! this might crash. Gonna need a good ol' testing.
-    char null_term = '\0';
-    memcpy(&header_element + 2 + string_length, &null_term, 1);
+    header header_element;
+    build_string_header(&header_element, key_s, string_length);
 
     return hashmap_set_internal(map, &header_element, raw_item);
 }
