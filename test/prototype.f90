60fc95d96687d10ddd49728d5c082a5178f50586
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-23 10:44:59 -0400

    the moment of truth

--- test/prototype.f90
+++ test/prototype.f90
@@ -33,7 +33,7 @@ contains
   !   type(c_ptr), intent(in), value :: raw_c_element
 
   !   if (.not. c_associated(raw_c_element)) then
-      
+
   !   end if
 
   !   select type (el)
@@ -55,88 +55,86 @@ program prototype
   implicit none
 
   type(hashmap_string_key) :: map
-  type(cool), pointer :: test_data
-  integer(c_int) :: i,z
-  integer(c_size_t) :: index
-  class(*), pointer :: generic_pointer
+  ! type(cool), pointer :: test_data
+  ! integer(c_int) :: i,z
+  ! integer(c_size_t) :: index
+  ! class(*), pointer :: generic_pointer
 
-  z = 0
-
-  !* Bunch of debug info so you can see it in action.
-
-  do
-
-    !* Create the hashmap.
-    map = new_hashmap_string_key(example_gc_function)
-
-    ! print*,"stage 1"
-    do i = 1+z,5000+z
-
-      !* Create our memory.
-      allocate(test_data)
-      allocate(test_data%i)
-
-      test_data%i = i
-
-      !* Put it into the hashmap.
-      !*
-      !* Uses memcpy under the hood.
-      call map%set("hi"//int_to_string(i), test_data)
-
-      !* Example getting.
-      if (map%get("hi"//int_to_string(i), generic_pointer)) then
-        ! print*,"got you"
-        select type (generic_pointer)
-         type is (cool)
-          ! print*,generic_pointer%i
-        end select
-      end if
-    end do
-
-    !* MAKE SURE, that your iterator starts at 0.
-    index = 0
-
-    ! print*,"stage 2"
-
-    !* Iterate the hashmap.
-    !*
-    !* NEVER delete elements while iterating!
-    do while(map%iterate(index, generic_pointer))
-      select type(generic_pointer)
-       type is (cool)
-        ! print*,generic_pointer%i
-      end select
-    end do
+  ! z = 0
 
 
-    !* We can remove the elements we just added because we already knew their keys.
-    !*
-    !* Will automatically call your GC function.
-    ! do i = 1+z,5000+z
-    !   call map%delete(int(i, c_int64_t))
-    ! end do
 
-    !* But if you want to be thorough and wipe the hashmap, you can simply clear it.
-    !*
-    !* Will automatically call your GC function.
-    ! call map%clear()
+  !* Create the hashmap.
+  map = new_hashmap_string_key(sizeof(10))
 
-    ! print*,"stage 3"
+  ! ! print*,"stage 1"
+  ! do i = 1+z,5000+z
 
-    !* We can finally free the hashmap.
-    !*
-    !* This WILL DESTROY the underlying C hashmap data!
-    !* DO NOT use the hashmap after this is called, until you have created it again.
-    !*
-    !* Will automatically call your GC function.
-    call map%free()
+  !   !* Create our memory.
+  !   allocate(test_data)
+  !   allocate(test_data%i)
 
-    ! print*,"nap time"
+  !   test_data%i = i
 
-    !* I just thought it would be neat to let you see it print out chunk by chunk.
-    ! call sleep(1)
+  !   !* Put it into the hashmap.
+  !   !*
+  !   !* Uses memcpy under the hood.
+  !   call map%set("hi"//int_to_string(i), test_data)
 
-    z = z + 5000
-  end do
+  !   !* Example getting.
+  !   if (map%get("hi"//int_to_string(i), generic_pointer)) then
+  !     ! print*,"got you"
+  !     select type (generic_pointer)
+  !      type is (cool)
+  !       ! print*,generic_pointer%i
+  !     end select
+  !   end if
+  ! end do
+
+  ! !* MAKE SURE, that your iterator starts at 0.
+  ! index = 0
+
+  ! ! print*,"stage 2"
+
+  ! !* Iterate the hashmap.
+  ! !*
+  ! !* NEVER delete elements while iterating!
+  ! do while(map%iterate(index, generic_pointer))
+  !   select type(generic_pointer)
+  !    type is (cool)
+  !     ! print*,generic_pointer%i
+  !   end select
+  ! end do
+
+
+  ! !* We can remove the elements we just added because we already knew their keys.
+  ! !*
+  ! !* Will automatically call your GC function.
+  ! ! do i = 1+z,5000+z
+  ! !   call map%delete(int(i, c_int64_t))
+  ! ! end do
+
+  ! !* But if you want to be thorough and wipe the hashmap, you can simply clear it.
+  ! !*
+  ! !* Will automatically call your GC function.
+  ! ! call map%clear()
+
+  ! ! print*,"stage 3"
+
+  ! !* We can finally free the hashmap.
+  ! !*
+  ! !* This WILL DESTROY the underlying C hashmap data!
+  ! !* DO NOT use the hashmap after this is called, until you have created it again.
+  ! !*
+  ! !* Will automatically call your GC function.
+  ! call map%free()
+
+  ! ! print*,"nap time"
+
+  ! !* I just thought it would be neat to let you see it print out chunk by chunk.
+  ! ! call sleep(1)
+
+  ! z = z + 5000
+! end do
 
 end program prototype
