c498002685ba74f7f3e05836ad8d5d5541f2f1e2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-23 05:49:12 -0400

    showcase how I figured this nonsense out

--- src/test_this.c
+++ src/test_this.c
@@ -1,2 +1,9 @@
+#include <stdio.h>
 #include "hashmap.c"
 
+int main()
+{
+  printf("size: %li\n", sizeof(element));
+  printf("len: %li\n", strlen("This is an example of how long your string key can be. If you don't think this is long enough then I think you might want to rethink your implementation. I still need to type something here :)"));
+  return 0;
+}
