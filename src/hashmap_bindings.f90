3bfadaee051c09765225b42885c58406a0887e08
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-23 10:38:34 -0400

    whole thing is wrong now

--- src/hashmap_bindings.f90
+++ src/hashmap_bindings.f90
@@ -34,89 +34,83 @@ module hashmap_bindings
     end function internal_hashmap_count
 
 
-    function internal_hashmap_set(map, item) result(void_pointer) bind(c, name = "hashmap_set")
+    function internal_hashmap_set_str_key(map, key_s, string_length, raw_item) result(void_pointer) bind(c, name = "hashmap_set_str_key")
       use, intrinsic :: iso_c_binding
       implicit none
 
-      type(c_ptr), intent(in), value :: map, item
+      type(c_ptr), intent(in), value :: map, raw_item
+      character(len = 1, kind = c_char), intent(in) :: key_s
+      integer(c_size_t), intent(in), value :: string_length
       type(c_ptr) :: void_pointer
-    end function internal_hashmap_set
+    end function internal_hashmap_set_str_key
 
 
-    function internal_hashmap_get(map, key) result(void_pointer) bind(c, name = "hashmap_get")
+!     function internal_hashmap_get(map, key) result(void_pointer) bind(c, name = "hashmap_get")
+!       use, intrinsic :: iso_c_binding
+!       implicit none
+
+!       type(c_ptr), intent(in), value :: map, key
+!       type(c_ptr) :: void_pointer
+!     end function internal_hashmap_get
+
+
+!     function internal_hashmap_delete(map, key) result(void_pointer) bind(c, name = "hashmap_delete")
+!       use, intrinsic :: iso_c_binding
+!       implicit none
+
+!       type(c_ptr), intent(in), value :: map, key
+!       type(c_ptr) :: void_pointer
+!     end function internal_hashmap_delete
+
+
+!     subroutine internal_hashmap_clear(map, update_capacity) bind(c, name = "hashmap_clear")
+!       use, intrinsic :: iso_c_binding
+!       implicit none
+
+!       type(c_ptr), intent(in), value :: map
+!       logical(c_bool), intent(in), value :: update_capacity
+!     end subroutine internal_hashmap_clear
+
+
+!     function internal_hashmap_iter(map, i, item) result(has_item) bind(c, name = "hashmap_iter")
+!       use, intrinsic :: iso_c_binding
+!       implicit none
+
+!       type(c_ptr), intent(in), value :: map
+!       integer(c_size_t), intent(inout) :: i
+!       type(c_ptr), intent(inout) :: item
+!       logical(c_bool) :: has_item
+!     end function internal_hashmap_iter
+
+
+! !? FUNCTION BLUEPRINTS. ===========================================================================
+
+
+!     recursive function hash_function_c_interface(item_pointer, seed_0, seed_1) result(hash) bind(c)
+!       use, intrinsic :: iso_c_binding
+!       implicit none
+
+!       type(c_ptr), intent(in), value :: item_pointer
+!       integer(c_int64_t), intent(in), value :: seed_0, seed_1
+!       integer(c_int64_t) :: hash
+!     end function hash_function_c_interface
+
+
+!     recursive function compare_function_c_interface(a, b, udata) result(comparitor) bind(c)
+!       use, intrinsic :: iso_c_binding
+!       implicit none
+
+!       type(c_ptr), intent(in), value :: a, b, udata
+!       logical(c_bool) :: comparitor
+!     end function compare_function_c_interface
+
+
+    subroutine gc_function_interface(raw_c_element)
       use, intrinsic :: iso_c_binding
       implicit none
 
-      type(c_ptr), intent(in), value :: map, key
-      type(c_ptr) :: void_pointer
-    end function internal_hashmap_get
-
-
-    function internal_hashmap_delete(map, key) result(void_pointer) bind(c, name = "hashmap_delete")
-      use, intrinsic :: iso_c_binding
-      implicit none
-
-      type(c_ptr), intent(in), value :: map, key
-      type(c_ptr) :: void_pointer
-    end function internal_hashmap_delete
-
-
-    subroutine internal_hashmap_clear(map, update_capacity) bind(c, name = "hashmap_clear")
-      use, intrinsic :: iso_c_binding
-      implicit none
-
-      type(c_ptr), intent(in), value :: map
-      logical(c_bool), intent(in), value :: update_capacity
-    end subroutine internal_hashmap_clear
-
-
-    function internal_hashmap_iter(map, i, item) result(has_item) bind(c, name = "hashmap_iter")
-      use, intrinsic :: iso_c_binding
-      implicit none
-
-      type(c_ptr), intent(in), value :: map
-      integer(c_size_t), intent(inout) :: i
-      type(c_ptr), intent(inout) :: item
-      logical(c_bool) :: has_item
-    end function internal_hashmap_iter
-
-
-!? FUNCTION BLUEPRINTS. ===========================================================================
-
-
-    recursive function hash_function_c_interface(item_pointer, seed_0, seed_1) result(hash) bind(c)
-      use, intrinsic :: iso_c_binding
-      implicit none
-
-      type(c_ptr), intent(in), value :: item_pointer
-      integer(c_int64_t), intent(in), value :: seed_0, seed_1
-      integer(c_int64_t) :: hash
-    end function hash_function_c_interface
-
-
-    recursive function compare_function_c_interface(a, b, udata) result(comparitor) bind(c)
-      use, intrinsic :: iso_c_binding
-      implicit none
-
-      type(c_ptr), intent(in), value :: a, b, udata
-      logical(c_bool) :: comparitor
-    end function compare_function_c_interface
-
-
-    subroutine gc_function_interface_string(el)
-      use :: hashmap_types
-      implicit none
-
-      class(*), pointer :: el
-    end subroutine gc_function_interface_string
-
-
-    subroutine gc_function_interface_integer(el)
-      use :: hashmap_types
-      implicit none
-
-      class(*), pointer :: el
-    end subroutine gc_function_interface_integer
+      type(c_ptr), intent(in), value :: raw_c_element
+    end subroutine gc_function_interface
 
 
   end interface
