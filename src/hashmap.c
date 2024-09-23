028d51feb0f9362c16d5e812471bdf8c8f0993ee
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-23 03:31:31 -0400

    these no longer exist

--- src/hashmap.c
+++ src/hashmap.c
@@ -48,11 +48,6 @@ const void *hashmap_delete(struct hashmap *map, const void *item);
 const void *hashmap_probe(struct hashmap *map, uint64_t position);
 bool hashmap_scan(struct hashmap *map, bool (*iter)(const void *item, void *udata), void *udata);
 bool hashmap_iter(struct hashmap *map, size_t *i, void **item);
-
-uint64_t hashmap_sip(const void *data, size_t len, uint64_t seed0, uint64_t seed1);
-uint64_t hashmap_murmur(const void *data, size_t len, uint64_t seed0, uint64_t seed1);
-uint64_t hashmap_xxhash3(const void *data, size_t len, uint64_t seed0, uint64_t seed1);
-
 const void *hashmap_get_with_hash(struct hashmap *map, const void *key, uint64_t hash);
 const void *hashmap_delete_with_hash(struct hashmap *map, const void *key, uint64_t hash);
 const void *hashmap_set_with_hash(struct hashmap *map, const void *item, uint64_t hash);
