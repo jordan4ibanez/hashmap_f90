b29b80a8f0511f678cd4474a2015c5501b95e6c8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-23 08:49:28 -0400

    fix this

--- src/test_this.c
+++ src/test_this.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-  printf("size: %li\n", sizeof(element));
+  printf("size: %li\n", sizeof(header));
   printf("len: %li\n", strlen("This is an example of how long your string key can be. If you don't think this is long enough then I think you might want to rethink your implementation. I still need to type something here :)"));
   return 0;
 }
